e6b62a7e17fa93d0b57fdf5033c23260f6fe3706
SimoneFranzoni <simofranzoni99@gmail.com>
2021-10-26 16:20:54 +0200

    Finish

--- js/script.js
+++ js/script.js
@@ -5,24 +5,17 @@ for(let i = 1; i <= 100; i++) {
     const box = document.createElement('div');    
     box.className = 'box';
     row.append(box);
-
-    if((i % 3 == 0)){
-        box.classList.add('div3');
-        box.innerHTML = 'fizz';
-    }
-
-    else if((i % 5 == 0)){
-        box.classList.add('div5');
-        box.innerHTML = 'buzz';
-    }
-
-    else {
-        box.innerHTML = i;
-    }
+    box.innerHTML = i;
 
     if((i % 5 == 0) && (i % 3 == 0)){
         box.classList.add('divboth')
         box.innerHTML = 'fizzbuzz';
-    }    
+    } else if((i % 3 == 0)){
+        box.classList.add('div3');
+        box.innerHTML = 'fizz';
+    } else if((i % 5 == 0)){
+        box.classList.add('div5');
+        box.innerHTML = 'buzz';
+    }
 
 };
