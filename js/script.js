e3059fdd1019fe34ffda8f0e3969c7218280f9da
SimoneFranzoni <simofranzoni99@gmail.com>
2021-10-26 15:38:03 +0200

    else-if-version

--- js/script.js
+++ js/script.js
@@ -4,7 +4,6 @@ const row = document.querySelector('.row');
 for(let i = 1; i <= 100; i++) {
     const box = document.createElement('div');    
     box.className = 'box';
-    box.innerHTML = i;
     row.append(box);
 
     if((i % 3 == 0)){
@@ -12,13 +11,18 @@ for(let i = 1; i <= 100; i++) {
         box.innerHTML = 'fizz';
     }
 
-    if((i % 5 == 0)){
+    else if((i % 5 == 0)){
         box.classList.add('div5');
         box.innerHTML = 'buzz';
+    }
+
+    else {
+        box.innerHTML = i;
     }
 
     if((i % 5 == 0) && (i % 3 == 0)){
         box.classList.add('divboth')
         box.innerHTML = 'fizzbuzz';
-    }
+    }    
+
 };
